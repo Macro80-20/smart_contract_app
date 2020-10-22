d45c47267a9bdfd622d2c6ee2c5e5f5ed3a4546b
Macro80-20 <fernando_lopezc@live.com>
2020-10-22 23:37:21 +0100

    fix eslint

--- deploy.js
+++ deploy.js
@@ -1,9 +1,9 @@
 // take some compiled code and deploy to a network
-const compiledFile = require("./compile");
+const compiledFile = require('./compile');
 
-const interface = compiledFile.abi;
+const contractInterface = compiledFile.abi;
 const bytecode = compiledFile.evm.bytecode.object;
 const metaData = compiledFile.metadata;
-console.log("Bytecode", bytecode);
-console.log("Interface", interface);
-console.log("MetaData", JSON.parse(metaData));
+console.log('Bytecode', bytecode);
+console.log('Interface', contractInterface);
+console.log('MetaData', JSON.parse(metaData));
