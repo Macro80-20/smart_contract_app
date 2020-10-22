8c562377ae742b5192e0e522cccc4f7ff477435f
Macro80-20 <fernando_lopezc@live.com>
2020-10-22 23:35:28 +0100

    add eslint

--- compile.js
+++ compile.js
@@ -1,28 +1,28 @@
 // sole prupose to compile the contracrt code
-const path = require("path");
-const fs = require("fs");
-const solc = require("solc");
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
 // his is going to generate a path that points directly to the inboxfile
-const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 
 // now we have our path we will read the raw code
-const source = fs.readFileSync(inboxPath, "utf8");
+const source = fs.readFileSync(inboxPath, 'utf8');
 
 const input = {
-  language: "Solidity",
+  language: 'Solidity',
   sources: {
-    "Inbox.sol": {
+    'Inbox.sol': {
       content: source,
     },
   },
   settings: {
     outputSelection: {
-      "*": {
-        "*": ["*"],
+      '*': {
+        '*': ['*'],
       },
     },
   },
 };
 const output = JSON.parse(solc.compile(JSON.stringify(input)));
-console.log(output.contracts["Inbox.sol"].Inbox.metadata);
-module.exports = output.contracts["Inbox.sol"].Inbox;
+console.log(output.contracts['Inbox.sol'].Inbox.metadata);
+module.exports = output.contracts['Inbox.sol'].Inbox;
